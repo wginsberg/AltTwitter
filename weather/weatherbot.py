cc064c49fb2190f94fdeffde3fe8844066a85072
Will Ginsberg <will.j.ginsberg@gmail.com>
2017-03-19 19:02:54 -0400

    Use absolute paths

--- weather/weatherbot.py
+++ weather/weatherbot.py
@@ -1,23 +1,22 @@
+#!/usr/bin/env python
+
 # imports
-import os 
+import sys
+import os
 from twitter import *
 import random
 
 # get credentials
-credentials = open('creds.txt')
+credentials = open(os.path.join(sys.path[0], 'creds.txt'))
 consumer_key, consumer_secret = credentials.read()[:-1].split('\n')
 credentials.close()
 
-print consumer_key
-print consumer_secret
-
 # authenticate app
-OAUTH = './oauth'
+OAUTH = os.path.join(sys.path[0], 'oauth')
 if not os.path.exists(OAUTH): oauth_dance("Alt Weather Bot", consumer_key, consumer_secret, OAUTH)
 token, secret = read_token_file(OAUTH)
 twitter = Twitter(auth=OAuth(token, secret, consumer_key, consumer_secret))
 
 temp = random.randrange(-350, 350)/10.0
 status = "It's %s degrees outside right now" % str(temp)
-
 twitter.statuses.update(status=status)
